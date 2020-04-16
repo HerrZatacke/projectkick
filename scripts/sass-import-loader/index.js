4c044c354f53a51bc690d0c47a4526aef49f5b3e
Herr Zatacke <zatacke@gmail.com>
2020-04-16 19:08:44 +0200

    eslint rules applied

--- scripts/sass-import-loader/index.js
+++ scripts/sass-import-loader/index.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-console */
 const conf = require('../../package').projectConfig;
 
-const vars = Object.keys(conf).map(key => (
+const vars = Object.keys(conf).map((key) => (
   `$CONFIG_${key}: ${conf[key]};`
 )).join('\n');
 
@@ -8,7 +9,7 @@ console.info('available sass variables from package.json [projectConfig]');
 console.info(vars);
 console.info('\n');
 
-const sassImportLoader = source => (
+const sassImportLoader = (source) => (
   `${vars}\n@import 'src/scss/imports/variables';\n@import 'src/scss/imports/mixins';\n${source}`
 );
 
